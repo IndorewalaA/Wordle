57cddec00d36abfe76ad0c1c819796035c2d7044
IndorewalaA <abdul.indorewala@ufl.edu>
2024-04-27 19:48:29 -0400

    added words list and some minor changes

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ def main_menu(screen: pygame.Surface):
     screen.fill((18, 18, 19))
     title_font = pygame.font.Font("fonts\FranklinGothic.ttf",70)
     play_font = pygame.font.Font("fonts\QUICK-ZIP.otf",30)
+
     title_card = title_font.render("Wordle", True, (255, 255, 255))
     title_block = title_card.get_rect(center=(200, 100))
     play_card = play_font.render("Play", True, (121, 121, 135))
     play_block = play_card.get_rect(center=(200, 300))
+
     screen.blit(title_card, title_block)
     screen.blit(play_card, play_block)
     pygame.display.update()
@@ -29,7 +31,16 @@ def main():
     pygame.init()
     main_menu(screen)
     while True:
-        franklin_gothic = pygame.font.Font("fonts\FranklinGothic.ttf",30)
+        franklin_gothic = pygame.font.Font("fonts\FranklinGothic.ttf",35)
+        small_title = franklin_gothic.render("Wordle", True, (255, 255, 255))
+        title_card = small_title.get_rect(center=(200, 50))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return
+            screen.fill((18, 18, 19))
+            screen.blit(small_title, title_card)
+            pygame.display.update()
 
 
 if __name__ == "__main__":
